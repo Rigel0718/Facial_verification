3edc77f6f4c97121135a0e5c9b2320e825ed1429
geonhee <pollux0718@gmail.com>
2023-07-04 17:59:26 +0000

    feat : add __main__ , transforms, confirm dataset.shape

--- Dataset.py
+++ Dataset.py
@@ -34,13 +34,15 @@ class CASIAWebFace(data.Dataset):
         self.image_list = image_list
         self.label_list = label_list
         self.class_nums = len(np.unique(self.label_list))  # label 중복제거
-        print("dataset size: ", len(self.image_list), '/','number_of_class', self.class_nums)
+        print("dataset size: ", len(self.image_list), '/','number_of_class : ', self.class_nums)
 
     def __getitem__(self, index) :
         img_path = self.image_list[index]
         label = self.label_list[index]
 
         img = self.loader(img_path)
+        
+        # transform
 
         if self.transform is not None :
             img = self.transform(img)
@@ -51,4 +53,18 @@ class CASIAWebFace(data.Dataset):
 
     def __len__(self) :
         return len(self.image_list)
+    
+if __name__ == '__main__' :
+    file_path = '/opt/ml/data/CASIA_WEBFAECE/CASIA-WebFace_crop'
 
+    transform = transforms.Compose([
+        transforms.ToTensor(),  # range [0, 255] -> [0.0, 1.0]
+        transforms.Normalize(mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5)) # range [0.0, 1.0] -> [-1.0,1.0]
+    ])
+
+    dataset = CASIAWebFace(file_path=file_path, transform=transform)
+    train_lodaer = data.DataLoader(dataset, batch_size=64, shuffle=True, num_workers=2, drop_last=False)
+    print(len(dataset))
+    for _data in train_lodaer :
+        print(_data[0].shape)  # dataset 확인
+        break
