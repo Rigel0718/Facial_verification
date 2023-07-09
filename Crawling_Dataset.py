9b5f026f5f8ef8c806b6348e2958a3d05617c195
geonhee <pollux0718@gmail.com>
2023-07-09 17:56:44 +0000

    feat :making Crawling dataset

---
+++ Crawling_Dataset.py
@@ -0,0 +1,80 @@
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import Resize
+import random
+import cv2 
+import os
+import torch
+import numpy as np
+
+def get_test_path(TEST_FILE_PATH, label) :
+    positive_image = []
+    negative_image = []
+    for dirname,_, filenames in os.walk(TEST_FILE_PATH) :
+        for filename in filenames :
+            img_path = os.path.join(dirname,filename)
+            img_label = filename.split('_')[-2]
+            if img_label == label :
+                positive_image.append(img_path)
+            else :
+                negative_image.append(img_path)
+    
+    test_num = len(positive_image)
+    negative_path_num = np.random.choice(len(negative_image), test_num) 
+    test_negative_image = []
+
+    for idx in negative_path_num :
+        test_negative_image.append(negative_image[idx])
+    
+    return positive_image, test_negative_image
+
+
+
+
+def labels_NAME2NUM(set_img_label):
+    label_dict = {}
+    for i, key in enumerate(set_img_label) :
+        label_dict[i] = key
+    return label_dict
+
+
+def get_images_labels(FILE_PAtH) :
+    image_path_list = []
+    set_image_label = set()
+    for dirname,_, filenames in os.walk(FILE_PAtH) :
+        for filename in filenames :
+            img_path = os.path.join(dirname,filename)
+            image_path_list.append(img_path)
+            _,img_label,label_num = filename.split('_')
+            set_image_label.add(img_label)
+    
+    return image_path_list, list(set_image_label)
+
+
+class Crawling_Dataset(Dataset) :
+    def __init__(self, Enrolled_FILE_PATH, TEST_FILE_PATH, transforms=None) :
+        _enrolled_file_path, _labels = get_images_labels(Enrolled_FILE_PATH)
+        _test_file_path, _test_labels = get_images_labels(TEST_FILE_PATH)
+        
+        self.enrolled_file_path = _enrolled_file_path
+        self.name_labels = _labels
+        self.num_labels_dict = labels_NAME2NUM(_labels)
+        self.transforms = transforms
+
+    def __len__(self) :
+        return len(self.enrolled_file_path)
+    
+    def get_label_dict(self) :
+        return self.num_labels_dict
+    
+    def __getitem__(self, index) :
+        image_path = self.enrolled_file_path[index]
+        image_label = image_path.split('_')[-2]
+        
+        if self.transforms is not None :
+            img = self.transforms(img)
+        else :
+            img = torch.from_numpy(img)
+        
+
+
+
--- evaluate.py
+++ evaluate.py
@@ -16,9 +16,20 @@ def distance(embedding1, embedding2, distance_metric='euclidian') :
     return dist
 
 def calculate_roc(thresholds, embedding1, embedding2, actual_issame, distance_metric, ss) :
-    assert (embedding1.shape[0]==embedding2.shape[0])
-    assert (embedding1.shape[1]==embedding2.shape[1])
+    assert (embedding1.shape[0]==embedding2.shape[0])  # 비교 batch
+    assert (embedding1.shape[1]==embedding2.shape[1])  #  embedding vector size 
     nrof_pairs = min(len(actual_issame), embedding1.shape[0])
     nrof_thresholds = len(thresholds)
 
-    tprs = np.zeros((nrof))
+    tprs = np.zeros((nrof_thresholds))  # true positive rate
+    fprs = np.zeros((nrof_thresholds))  # false positive rate
+    accuracy = 0   # 일단 만들어놈
+
+    is_false_positive = []
+    is_false_negative = []
+
+    indices = np.arange(nrof_pairs)  # batch에 들어가는 정답 pair들의 index설정
+
+     
+
+
