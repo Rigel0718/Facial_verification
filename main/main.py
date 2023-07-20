64fecc7a039f28753f537cfa2143a612280b325b
geonhee <pollux0718@gmail.com>
2023-07-20 15:43:01 +0000

    feat : main_detection

--- main/main.py
+++ main/main.py
@@ -1,2 +1,25 @@
 from facenet_pytorch import MTCNN, InceptionResnetV1, fixed_image_standardization
+from Facial_verification.utils.Embedding import Embedding_vector, Embeddings_Manager
+from Facial_verification.utils.Label_DataFrame import Label_DataFrame
+import cv2
+import numpy as np
 
+def img_loader(path) :
+    try : 
+        with open(path, 'rb') as f :
+            img = cv2.imread(path) 
+            if len(img.shape) == 2 :
+                img = np.stack([img] * 3, 2)
+        
+            return img
+    except IOError :
+        print('Cannot load image' + path)
+
+detection_model = MTCNN(image_size=160, margin=0, min_face_size=20, thresholds=[0.6,0.7,0.7], factor= 0.709, keep_all=True)
+img_path = '/opt/ml/data/example/group_less.jpg'
+img = img_loader(img_path)
+x = MTCNN(img)
+print(x.shape)
+
+extraction_model = InceptionResnetV1(classify=False, pretrained='vggface2')
+# enrolled_img_path = ''
