87a83f4cab0e2f5a1c6163ed972acd2f04153c0f
geonhee <pollux0718@gmail.com>
2023-07-20 16:24:59 +0000

    feat : facenet embedding vector instance

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from facenet_pytorch import MTCNN, InceptionResnetV1, fixed_image_standardization
-from utils.Embedding import Embedding_vector, Embeddings_Manager
+from utils.Embedding import Embedding_vector, Embeddings_Manager, img_loader
 from utils.Label_DataFrame import Label_DataFrame
 import cv2
 import numpy as np
 
-def img_loader(path) :
-    try : 
-        with open(path, 'rb') as f :
-            img = cv2.imread(path) 
-            if len(img.shape) == 2 :
-                img = np.stack([img] * 3, 2)
-        
-            return img
-    except IOError :
-        print('Cannot load image' + path)
-
-detection_model = MTCNN(image_size=160, margin=0, min_face_size=20, thresholds=[0.6,0.7,0.7], factor= 0.709, keep_all=True)
 img_path = '/opt/ml/data/example/group_less.jpg'
+
+# detection model 생성
+# keep_all을 제외하면 default
+detection_model = MTCNN(image_size=160, margin=0, min_face_size=20, thresholds=[0.6,0.7,0.7], factor= 0.709, keep_all=True) 
+
 img = img_loader(img_path)
 x = detection_model(img)
-print(x.shape)
+print(x.shape) # 5명인 경우 Tensor [5, 3, 160, 160]
 
-extraction_model = InceptionResnetV1(classify=False, pretrained='vggface2')
-# enrolled_img_path = ''
+# 모델 정의 
+facenet_model = InceptionResnetV1(classify=False, pretrained='vggface2')
+
+embedding_facenet = Embedding_vector(model=facenet_model)
+
