c7d1ae3162991b82a22158f766c56b98a64ff377
geonhee <pollux0718@gmail.com>
2023-08-08 13:35:04 +0000

    feat : image conver to jpeg

--- utils/check_image_file.py
+++ utils/check_image_file.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import os
 
+# 이미지 파일을 정상적으로 쓸 수 있는지 확인
 def check_image_file_integrity(directory_path):
     fail_list = []
     for directory, _, filename in os.walk(directory_path) :
@@ -14,3 +15,17 @@ def check_image_file_integrity(directory_path):
                 fail_list.append(file_path)
 
     return fail_list
+
+# 다른 파일을 JPEG 형태로 바꿈
+def convert_to_jpg(input_dir, output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+    for filename in os.listdir(input_dir):
+        if filename.lower().endswith(('.png', '.bmp', '.gif', '.tiff', '.jpeg')):
+            image_path = os.path.join(input_dir, filename)
+            image = Image.open(image_path)
+            if image.mode != 'RGB':
+                image = image.convert('RGB')
+            output_path = os.path.join(output_dir, os.path.splitext(filename)[0] + '.jpg')
+            image.save(output_path, 'JPEG')
+
+    
