14c17f8c054b9e5ee0769d00d4b7f01979941061
geonhee <pollux0718@gmail.com>
2023-08-18 15:39:18 +0000

    feat : config parse

--- train.py
+++ train.py
@@ -10,6 +10,8 @@ from tqdm.auto import tqdm
 import wandb
 import torch
 from torch import distributed
+import argparse
+import yaml
 
 from torchvision.transforms import Resize
 from facenet_pytorch import InceptionResnetV1, fixed_image_standardization
@@ -20,15 +22,26 @@ from test.Validation_statistic import validation
 from dataset.Crawling_Dataset import Crawling_Nomal_Dataset
 from utils.set_seed import setup_seed
 
-# setting
-save_file_name = 're_bat64_lr_1e-3'
-train_data_path = '/opt/ml/data/celeb/train'
-test_path = '/opt/ml/data/celeb/test'
-batch_size = 64
-total_epoch = 15
-root = '/opt/ml/result'
-wandb_log = True
-save_num = 5
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-c', '--config', help='Path to YAML configuration file', default='/opt/ml/Facial_verification/config.yaml')
+parser.add_argument('--save_file_name', type=str, default='re_bat64_lr_1e-3')
+parser.add_argument('--train_data_path', type=str, default='/opt/ml/data/celeb/train')
+parser.add_argument('--batch_size', type=int, default=64)
+parser.add_argument('--total_epoch', type=int, default=15)
+parser.add_argument('--save_num', type=int, default=5)
+parser.add_argument('--weight_decay', type=float, default=1e-4)
+parser.add_argument('--learning_rate', type=float, default=1e-3)
+parser.add_argument('--validation', type=bool, default=False)
+parser.add_argument('--seed_num', type=int, default=22)
+
+args = parser.parse_args()
+
+with open(args.config, 'r') as file :
+     config=yaml.safe_load(file)
+parser.set_defaults(**config)
+args = parser.parse_args()
+print('V : ', args)
 
 try:
     rank = int(os.environ["RANK"])
@@ -56,19 +69,19 @@ def save_model(model, epoch, file_name='test_model.pt', final=False):
 
 def train() :
 
-    setup_seed(22)
+    setup_seed(args.seed_num)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # log init
-    save_dir = os.path.join('./workspace/'+ save_file_name+ '_' + datetime.now().strftime('%Y%m%d_%H%M%S'))
+    save_dir = os.path.join('./workspace/'+ args.save_file_name+ '_' + datetime.now().strftime('%Y%m%d_%H%M%S'))
 
     os.makedirs(save_dir)
 
-    if wandb_log : 
-        wandb.init(entity='hi-ai',
-                   project='semi_hard_triplet',
-                   name=save_file_name)
+    if args.wandb_logging : 
+        wandb.init(entity=args.wandb_entity,
+                   project=args.wandb_project,
+                   name=args.save_file_name)
 
     transform = transforms.Compose([
         np.float32,
@@ -77,11 +90,11 @@ def train() :
         fixed_image_standardization
     ])
     
-    train_dataset = Crawling_Nomal_Dataset(train_data_path, transforms=transform)
-    trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
+    train_dataset = Crawling_Nomal_Dataset(args.train_data_path, transforms=transform)
+    trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size,
                                               shuffle=True, num_workers=8, drop_last=False)
     
-    test_dataset = Crawling_Nomal_Dataset(test_path, transforms=transform)
+    test_dataset = Crawling_Nomal_Dataset(args.test_path, transforms=transform)
     test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=2, drop_last=False)
 
 
@@ -112,8 +125,8 @@ def train() :
 
     best_acc = 0
 
-    for epoch in tqdm(range(1, total_epoch + 1), leave=True) :
-        exp_lr_scheduler.step()
+    for epoch in tqdm(range(1, args.total_epoch + 1), leave=True) :
+        
         model.train()
         progress_bar = enumerate(tqdm(trainloader))
 
@@ -129,17 +142,19 @@ def train() :
             output_loss = criterion(label, feature)
             output_loss.backward()
             optimizer_ft.step()
-
+            exp_lr_scheduler.step()
         # validation
-        model.eval()
-        accuracy, recall, f1, precision = validation(model, test_data_loader)  
-        if wandb_log :
-                wandb.log({'accuracy' : accuracy, 'recall' : recall, 'f1' : f1, 'precision' : precision})
-        if (epoch + 1) % save_num == 0 :
-            save_model(model, epoch ,save_dir)
-        if best_acc < accuracy :
-            best_acc = accuracy
-            save_model(model, epoch, save_dir, final=True)
+        if args.validation :
+            with torch.no_grad() :
+                model.eval()
+                accuracy, recall, f1, precision = validation(model, test_data_loader)  
+                if args.wandb_logging :
+                        wandb.log({'accuracy' : accuracy, 'recall' : recall, 'f1' : f1, 'precision' : precision})
+                if (epoch + 1) % args.save_num == 0 :
+                    save_model(model, epoch ,save_dir)
+                if best_acc < accuracy :
+                    best_acc = accuracy
+                    save_model(model, epoch, save_dir, final=True)
 
 if __name__ == '__main__' :
     train()
